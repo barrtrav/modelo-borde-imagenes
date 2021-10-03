4791a5e3fa72c7d242eb71fb361587092b367f70
ariel.plasencia <arielplasencia00@gmail.com>
2021-10-03 02:27:58 -0400

    Remove old files and folders

--- PyForm/__init__.py
+++
@@ -1,9 +0,0 @@
-from PyForm.edge_param import EdgeParam
-from PyForm.gauss_param import GaussParam
-from PyForm.threshold_param import ThresholdParam
-
-__all__ =[
-    'EdgeParam',
-    'GaussParam',
-    'ThresholdParam'
-]
