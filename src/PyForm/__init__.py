3b1b9c86f56f475a56188158df7983ff1cade26b
ariel.plasencia <arielplasencia00@gmail.com>
2021-10-03 02:27:04 -0400

    Add a project's structure

---
+++ src/PyForm/__init__.py
@@ -0,0 +1,9 @@
+from PyForm.edge_param import EdgeParam
+from PyForm.gauss_param import GaussParam
+from PyForm.threshold_param import ThresholdParam
+
+__all__ =[
+    'EdgeParam',
+    'GaussParam',
+    'ThresholdParam'
+]
