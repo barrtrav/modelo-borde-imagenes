9397544137df5f4ffff6d25e8780f2227012f308
ariel.plasencia <arielplasencia00@gmail.com>
2021-10-05 17:11:18 -0400

    add summary to class EdgeParam in src/PyForm/edge_param.py

--- src/PyForm/edge_param.py
+++ src/PyForm/edge_param.py
@@ -1,7 +1,10 @@
-from PyQt5.QtWidgets import QDialog
 from PyUi import Ui_Dialog_Edge
+from PyQt5.QtWidgets import QDialog
 
 class EdgeParam(QDialog, Ui_Dialog_Edge):
+    '''
+    Clase encargada de recopilar los parametros para aplicar el filtro edge.
+    '''
     def __init__(self):
         QDialog.__init__(self)
         self.setupUi(self)
@@ -20,25 +23,19 @@ class EdgeParam(QDialog, Ui_Dialog_Edge):
             self.radioButton_1.setEnabled(True)
             self.radioButton_2.setEnabled(True)
             self.radioButton_3.setEnabled(True)
-
             self.radioButton_2.setText('Positive')
-            self.radioButton_3.setText('Negative')
-        
+            self.radioButton_3.setText('Negative')        
         elif edgetype == 'Laplace':
             self.radioButton_1.setDisabled(True)
             self.radioButton_2.setDisabled(True)
             self.radioButton_3.setDisabled(True)
-
             self.spinBox.setEnabled(True)
-        
         else:
             self.radioButton_1.setEnabled(True)
             self.radioButton_2.setEnabled(True)
             self.radioButton_3.setEnabled(True)
-
             self.radioButton_2.setText('Horizontal')
             self.radioButton_3.setText('Vertical')
-
             self.spinBox.setDisabled(True)
 
     def AcceptedAction(self):
